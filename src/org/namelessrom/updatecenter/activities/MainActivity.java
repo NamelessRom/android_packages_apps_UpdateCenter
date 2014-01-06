97e57f698594793a4759fb52c0dff33e0fb94e45
Alexander Martinz <eviscerationls@gmail.com>
2014-01-06 03:51:13 +0100

    WORK IN PROGRESS
    Change-Id: I71fba0ad575cab4c61bf9161c853b2d94baac214

---
+++ src/org/namelessrom/updatecenter/activities/MainActivity.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2013 - 2014 Alexander "Evisceration" Martinz
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses
+ */
+
+package org.namelessrom.updatecenter.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import org.namelessrom.updatecenter.R;
+import org.namelessrom.updatecenter.fragments.CenterMainFragment;
+
+public class MainActivity extends Activity {
+
+
+    //
+    public static final String CENTER_MAIN_FRAGMENT_TAG = "center_main_fragment_tag";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            getFragmentManager().beginTransaction()
+                    .add(R.id.container, new CenterMainFragment(), CENTER_MAIN_FRAGMENT_TAG)
+                    .commit();
+        }
+    }
+
+    //
+
+
+    @Override
+    public void onBackPressed() {
+        boolean mCancelBackPress = false;
+
+        CenterMainFragment f = (CenterMainFragment)
+                getFragmentManager().findFragmentByTag(CENTER_MAIN_FRAGMENT_TAG);
+        if (f != null) {
+            mCancelBackPress = f.onFragmentBackPressed();
+        }
+
+        if (!mCancelBackPress) {
+            super.onBackPressed();
+        }
+    }
+}
