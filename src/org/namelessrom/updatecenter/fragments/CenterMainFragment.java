813b79556091436efefa1c77ee262f89e81f4f75
Alexander Martinz <eviscerationls@gmail.com>
2014-01-06 03:51:13 +0100

    WORK IN PROGRESS
    Change-Id: I71fba0ad575cab4c61bf9161c853b2d94baac214

---
+++ src/org/namelessrom/updatecenter/fragments/CenterMainFragment.java
@@ -0,0 +1,200 @@
+/*
+ * Copyright (C) 2013 - 2014 Alexander "Evisceration" Martinz
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses
+ */
+
+package org.namelessrom.updatecenter.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.ScaleAnimation;
+import android.widget.FrameLayout;
+
+import org.namelessrom.updatecenter.R;
+import org.namelessrom.updatecenter.utils.interfaces.FragmentInterface;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by alex on 05.01.14.
+ */
+public class CenterMainFragment extends Fragment implements View.OnClickListener,
+        Animation.AnimationListener, FragmentInterface {
+
+    //
+    private static final int ANIMATION_FIRST_STEP = 0;
+    private static final int ANIMATION_SECOND_STEP = 1;
+
+    //
+    private int mCurrentId = 0;
+    private Fragment mCurrentFragment;
+    private View mCurrentView;
+    // For flipping
+    private boolean mShowingBack = false;
+    // For zooming
+    private int mInitialHeight, mInitialWidth;
+    private int mAnimationStep = 0;
+
+    //
+    private View rootView;
+    private FrameLayout updateView, addonView, dummyView1, dummyView2;
+    private static List<View> viewList = new ArrayList<View>();
+
+    //
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        rootView = inflater.inflate(R.layout.fragment_center_main, container, false);
+
+        updateView = (FrameLayout) rootView.findViewById(R.id.frameLayout0);
+        updateView.setOnClickListener(this);
+
+        addonView = (FrameLayout) rootView.findViewById(R.id.frameLayout1);
+        addonView.setOnClickListener(this);
+
+        dummyView1 = (FrameLayout) rootView.findViewById(R.id.frameLayout2);
+        dummyView1.setOnClickListener(this);
+
+        dummyView2 = (FrameLayout) rootView.findViewById(R.id.frameLayout3);
+        dummyView2.setOnClickListener(this);
+
+        getChildFragmentManager()
+                .beginTransaction()
+                .replace(R.id.frameLayout0, new UpdateStaticFragment())
+                .commit();
+
+        viewList.add(updateView);
+        viewList.add(addonView);
+        viewList.add(dummyView1);
+        viewList.add(dummyView2);
+
+        return rootView;
+    }
+
+    @Override
+    public void onClick(View view) {
+        mCurrentView = view;
+        mCurrentId = view.getId();
+        switch (mCurrentId) {
+
+            case R.id.frameLayout0:
+                mCurrentFragment = new UpdateFragment();
+                break;
+
+            case R.id.frameLayout1:
+                mCurrentFragment = new AddonsFragment();
+                break;
+
+        }
+
+        flipCard();
+    }
+
+
+    //
+
+    public void flipCard() {
+
+        if (!mShowingBack) {
+            mInitialWidth = mCurrentView.getWidth();
+            mInitialHeight = mCurrentView.getHeight();
+            for (View v : viewList) {
+                if (!v.equals(mCurrentView)) {
+                    v.setVisibility(View.GONE);
+                }
+            }
+        }
+
+        ScaleAnimation animation = new ScaleAnimation(1, 0, 1, 0);
+        animation.setDuration(200);
+        animation.setAnimationListener(this);
+        mCurrentView.startAnimation(animation);
+
+    }
+
+    @Override
+    public void onAnimationEnd(Animation animation) {
+        switch (mAnimationStep) {
+            case ANIMATION_FIRST_STEP:
+
+                mAnimationStep = ANIMATION_SECOND_STEP;
+
+                mCurrentView.getLayoutParams().height =
+                        (mShowingBack ? mInitialHeight : rootView.getHeight());
+                mCurrentView.getLayoutParams().width =
+                        (mShowingBack ? mInitialWidth : rootView.getWidth());
+                mCurrentView.requestLayout();
+
+                animation = new ScaleAnimation(0, 1, 0, 1);
+                animation.setDuration(500);
+                animation.setAnimationListener(this);
+                mCurrentView.startAnimation(animation);
+                break;
+            case ANIMATION_SECOND_STEP:
+
+                mAnimationStep = ANIMATION_FIRST_STEP;
+
+                if (mShowingBack) {
+                    for (View v : viewList) {
+                        if (!v.equals(mCurrentView)) {
+                            v.setVisibility(View.VISIBLE);
+                        }
+                    }
+                    mShowingBack = false;
+                    getChildFragmentManager().popBackStack();
+                    return;
+                }
+
+                getChildFragmentManager()
+                        .beginTransaction()
+                        .setCustomAnimations(
+                                R.animator.card_flip_right_in, R.animator.card_flip_right_out,
+                                R.animator.card_flip_left_in, R.animator.card_flip_left_out)
+                        .replace(mCurrentId, mCurrentFragment)
+                        .addToBackStack(null)
+                        .commit();
+
+                mShowingBack = true;
+
+                break;
+        }
+    }
+
+    @Override
+    public void onAnimationRepeat(Animation animation) {
+
+    }
+
+    @Override
+    public void onAnimationStart(Animation animation) {
+
+    }
+
+    @Override
+    public boolean onFragmentBackPressed() {
+        if (getChildFragmentManager().getBackStackEntryCount() > 0) {
+            flipCard();
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
---
+++ src/org/namelessrom/updatecenter/fragments/UpdateFragment.java
@@ -0,0 +1,235 @@
+/*
+ * Copyright (C) 2013 - 2014 Alexander "Evisceration" Martinz
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses
+ */
+
+package org.namelessrom.updatecenter.fragments;
+
+import android.app.Dialog;
+import android.app.ListFragment;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.namelessrom.updatecenter.R;
+import org.namelessrom.updatecenter.utils.Helper;
+import org.namelessrom.updatecenter.utils.adapters.UpdateListAdapter;
+import org.namelessrom.updatecenter.utils.classes.HttpHandler;
+import org.namelessrom.updatecenter.utils.items.UpdateListItem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by alex on 05.01.14.
+ */
+public class UpdateFragment extends ListFragment {
+
+    //
+    private static final String URL = "http://nameless-rom.org:3000/api";
+    private static final String CHANNEL_NIGHTLY = "NIGHTLY";
+    // JSON Node names
+    private static final String TAG_CHANNEL = "channel";
+    private static final String TAG_FILENAME = "filename";
+    private static final String TAG_MD5SUM = "md5sum";
+    private static final String TAG_TIMESTAMP = "timestamp";
+    // private static final String TAG_CODENAME = "codename";
+    private static final String TAG_CHANGELOG = "changelog";
+    private static final String TAG_URL = "url";
+    //private static final String TAG_ID = "_id";
+    //
+    JSONArray mUpdateArray = null;
+
+    //
+    List<UpdateListItem> mTitles = new ArrayList<UpdateListItem>();
+    List<UpdateListItem> mTmpTitles = new ArrayList<UpdateListItem>();
+
+    @Override
+    public View onCreateView(LayoutInflater layoutInflater, ViewGroup viewGroup, Bundle bundle) {
+        return super.onCreateView(layoutInflater, viewGroup, bundle);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle bundle) {
+        super.onViewCreated(view, bundle);
+        new CheckUpdateTask().execute();
+    }
+
+    @Override
+    public void onListItemClick(ListView listView, View view, int i, long l) {
+        super.onListItemClick(listView, view, i, l);
+        if (i == 0) {
+            new CheckUpdateTask().execute();
+        } else {
+            showUpdateDialog(mTitles.get(i));
+        }
+    }
+
+    private void showUpdateDialog(UpdateListItem updateListItem) {
+        if (updateListItem == null) return;
+
+        final Dialog dialog = new Dialog(getActivity());
+        dialog.setContentView(R.layout.dialog_updates);
+        dialog.setTitle(getString(R.string.update_title));
+
+        TextView text = (TextView) dialog.findViewById(R.id.dialog_updates_info);
+        String tmp = getString(R.string.update_name, updateListItem.getUpdateName()) + "\n";
+        tmp += getString(R.string.update_channel, updateListItem.getUpdateChannel()) + "\n";
+        tmp += getString(R.string.update_timestamp, updateListItem.getUpdateTimeStamp()) + "\n";
+        tmp += getString(R.string.update_info, updateListItem.getUpdateInfo()) + "\n";
+        tmp += getString(R.string.update_changelog, updateListItem.getUpdateChangeLog()) + "\n";
+        text.setText(tmp);
+
+        Button dialogButton = (Button) dialog.findViewById(R.id.dialogButtonOK);
+        dialogButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+
+        dialog.show();
+    }
+
+//
+
+    private void getTitles() {
+        UpdateListItem item = new UpdateListItem(
+                "---", getString(R.string.general_press_to_update), ""
+        );
+        mTitles.add(item);
+
+        for (UpdateListItem mTmpTitle : mTmpTitles) {
+            mTitles.add(mTmpTitle);
+        }
+
+        UpdateListAdapter adapter = new UpdateListAdapter(getActivity(), mTitles);
+
+        setListAdapter(adapter);
+        adapter.notifyDataSetChanged();
+    }
+
+    //
+
+    class CheckUpdateTask extends AsyncTask<Void, Void, Void> {
+
+        final Context mContext = getActivity();
+        ProgressDialog mDialog = new ProgressDialog(mContext);
+
+        @Override
+        protected void onPreExecute() {
+            // Show dialog
+            mDialog.setTitle("Checking for updates");
+            mDialog.setMessage("Searching for Updates on Channel \"" + CHANNEL_NIGHTLY + "\"");
+            mDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+            mDialog.setCancelable(true);
+            mDialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
+                @Override
+                public void onCancel(DialogInterface dialogInterface) {
+                    CheckUpdateTask.this.cancel(true);
+                }
+            });
+            mDialog.show();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+
+            if (mTitles != null) {
+                mTitles.clear();
+            } else {
+                mTitles = new ArrayList<UpdateListItem>();
+            }
+            if (mTmpTitles != null) {
+                mTmpTitles.clear();
+            } else {
+                mTmpTitles = new ArrayList<UpdateListItem>();
+            }
+
+            // Builds: BaseUrl + Channel + DeviceId
+            String url = URL + "/" + CHANNEL_NIGHTLY + "/" + Helper.getDeviceId();
+            Log.e("HttpHandler", "Url: " + url);
+
+            HttpHandler httpHandler = new HttpHandler();
+            String jsonStr = httpHandler.sendRequest(url, HttpHandler.GET);
+
+            if (jsonStr != null) {
+                try {
+                    // Getting JSON Array node
+                    mUpdateArray = new JSONArray(jsonStr);
+
+                    if (mUpdateArray.length() == 0) {
+                        CheckUpdateTask.this.cancel(true);
+                        return null;
+                    }
+
+                    for (int i = 0; i < mUpdateArray.length(); i++) {
+                        JSONObject c = mUpdateArray.getJSONObject(i);
+
+                        //final String id = c.getString(TAG_ID);
+                        String channel = c.getString(TAG_CHANNEL);
+                        final String filename = c.getString(TAG_FILENAME).replace(".zip", "");
+                        final String md5sum = c.getString(TAG_MD5SUM);
+                        final String urlFile = c.getString(TAG_URL);
+                        final String timeStamp = c.getString(TAG_TIMESTAMP);
+                        final String changeLog = c.getString(TAG_CHANGELOG);
+
+                        UpdateListItem item = new UpdateListItem(channel, filename, md5sum,
+                                urlFile, timeStamp, changeLog);
+
+                        mTmpTitles.add(item);
+                    }
+                } catch (JSONException e) {
+                    CheckUpdateTask.this.cancel(true);
+                    e.printStackTrace();
+                }
+            } else {
+                CheckUpdateTask.this.cancel(true);
+                Log.e("HttpHandler", "Couldn't get any data from the url");
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onCancelled() {
+            mDialog.hide();
+            mTmpTitles.clear();
+            mTmpTitles.add(
+                    new UpdateListItem("-", getString(R.string.general_no_updates_available), "")
+            );
+            getTitles();
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            mDialog.hide();
+            getTitles();
+        }
+
+    }
+}
