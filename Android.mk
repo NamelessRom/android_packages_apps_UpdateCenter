6c7370320076a17439e402696f4a77e7a18096fe
Evisceration <eviscerationls@gmail.com>
2014-03-08 22:42:23 +0100

    UC: rewrite to use changeloglib for displaying changelogs
    Change-Id: I34abb85bddfacc0f4fff669dcca627f848f91682

--- Android.mk
+++ Android.mk
@@ -4,14 +4,22 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
 
-#LOCAL_STATIC_JAVA_LIBRARIES := android-support-v13
 LOCAL_JAVA_LIBRARIES := okhttp
+LOCAL_STATIC_JAVA_LIBRARIES := changeloglib
 
-LOCAL_SRC_FILES := $(call all-java-files-under, src)
+LOCAL_SRC_FILES := \
+    $(call all-java-files-under, src)
+
+changelog_dir := ../../../external/changeloglib/res
+res_dirs := res $(changelog_dir)
+LOCAL_RESOURCE_DIR := $(addprefix $(LOCAL_PATH)/, $(res_dirs))
 
 LOCAL_PACKAGE_NAME := UpdateCenter
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
+
+LOCAL_AAPT_FLAGS := --auto-add-overlay
+LOCAL_AAPT_FLAGS += --extra-packages com.android.changelibs
 
 include $(BUILD_PACKAGE)
 
