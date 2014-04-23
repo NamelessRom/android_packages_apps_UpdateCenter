504ec98f0a25247885fe020b9cfe89e9d7ae0255
Evisceration <eviscerationls@gmail.com>
2014-04-23 23:28:25 +0200

    ...
    Change-Id: I80e9aadb80e02f98c57aac96b2c120876687dc28

---
+++ ion/src/com/koushikdutta/ion/cookie/CookieMiddleware.java
@@ -0,0 +1,108 @@
+package com.koushikdutta.ion.cookie;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.text.TextUtils;
+import android.util.Log;
+
+import com.koushikdutta.async.http.Multimap;
+import com.koushikdutta.async.http.SimpleMiddleware;
+import com.koushikdutta.async.http.libcore.RawHeaders;
+
+import java.net.CookieManager;
+import java.net.CookieStore;
+import java.net.HttpCookie;
+import java.net.URI;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by koush on 5/29/13.
+ */
+public class CookieMiddleware extends SimpleMiddleware {
+    CookieManager manager;
+    SharedPreferences preferences;
+
+    public CookieStore getCookieStore() {
+        return manager.getCookieStore();
+    }
+
+    public void clear() {
+        getCookieStore().removeAll();
+        preferences.edit().clear().commit();
+    }
+
+    public CookieManager getCookieManager() {
+        return manager;
+    }
+
+    public CookieMiddleware(Context context, String name) {
+        reinit(context, name);
+    }
+
+    public void reinit(Context context, String name) {
+        manager = new CookieManager(null, null);
+        preferences = context.getSharedPreferences(name + "-cookies", Context.MODE_PRIVATE);
+
+        Map<String, ?> allPrefs = preferences.getAll();
+        for (String key: allPrefs.keySet()) {
+            try {
+                String value = preferences.getString(key, null);
+                RawHeaders headers = new RawHeaders();
+                String[] lines = value.split("\n");
+                boolean first = true;
+                for (String line: lines) {
+                    if (first) {
+                        first = false;
+                        headers.setStatusLine(line);
+                    }
+                    else if (!TextUtils.isEmpty(line)) {
+                        headers.addLine(line);
+                    }
+                }
+                manager.put(URI.create(key), headers.toMultimap());
+            }
+            catch (Exception e) {
+                Log.e("Ion", "unable to load cookies", e);
+            }
+        }
+    }
+
+    @Override
+    public void onSocket(OnSocketData data) {
+        try {
+            Map<String, List<String>> cookies = manager.get (data.request.getUri(), data.request.getHeaders().getHeaders().toMultimap());
+            data.request.getHeaders().addCookies(cookies);
+        }
+        catch (Exception e) {
+        }
+    }
+
+    @Override
+    public void onHeadersReceived(OnHeadersReceivedData data) {
+        put(data.request.getUri(), data.headers.getHeaders());
+    }
+
+    public void put(URI uri, RawHeaders headers) {
+        try {
+            manager.put(uri, headers.toMultimap());
+
+            // no cookies to persist.
+            if (headers.get("Set-Cookie") == null)
+                return;
+
+            List<HttpCookie> cookies = manager.getCookieStore().get(uri);
+
+            RawHeaders dump = new RawHeaders();
+            dump.setStatusLine("HTTP/1.1 200 OK");
+            for (HttpCookie cookie: cookies) {
+                dump.add("Set-Cookie", cookie.getName() + "=" + cookie.getValue());
+            }
+
+            String key = uri.getScheme() + "://" + uri.getAuthority();
+            preferences.edit().putString(key, dump.toHeaderString()).commit();
+        }
+        catch (Exception e) {
+        }
+    }
+}
