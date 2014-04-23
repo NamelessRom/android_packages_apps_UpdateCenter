a2989708a8a8c75827eb9ee96046cbee5806acd4
Evisceration <eviscerationls@gmail.com>
2014-04-23 23:28:25 +0200

    UC: big rework
    drop volley for Ion by @koush
    temporary include a custom version, until the repositories got updated.
    
    rework layout, use a cards like optic.
    includes a custom app store, which menu is hidden.
    it will be unhidden once the server API side is fully ready.
    
    when pressing on an update, it opens the update in detail.
    the detailview has a symbol on the top left, indicating its channel.
    example: N for Nightly, M for Milestone, RC for Release Candidate, ...
    
    right of it is the date of the update, below of it the update name
    
    theres also a button, which knows 3 states.
    *) downloading, while the update is downloading, give the option to
    	cancel it
    *) downloaded, allows to automatically install the update
    *) not downloaded, allows to download the update
    
    below of it is a (hidden) progressbar, which in future updates shows
    the current download progress.
    
    and below that progress bar is a listview, showing the changelog.
    
    when long pressing on an update, it opens the changelog in a dialog.
    
    theres still a lot of work left, but this change brings us a lot closer.
    
    Change-Id: I80e9aadb80e02f98c57aac96b2c120876687dc28

---
+++ src/main/java/org/namelessrom/updatecenter/services/DownloadService.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright (C) 2012 The CyanogenMod Project
+ *
+ * * Licensed under the GNU GPLv2 license
+ *
+ * The text of the license can be found in the LICENSE file
+ * or at https://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+package org.namelessrom.updatecenter.services;
+
+import android.app.DownloadManager;
+import android.app.IntentService;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Parcelable;
+import android.preference.PreferenceManager;
+
+import org.namelessrom.updatecenter.Application;
+import org.namelessrom.updatecenter.R;
+import org.namelessrom.updatecenter.database.DatabaseHandler;
+import org.namelessrom.updatecenter.database.DownloadItem;
+import org.namelessrom.updatecenter.events.RefreshEvent;
+import org.namelessrom.updatecenter.utils.BusProvider;
+import org.namelessrom.updatecenter.utils.Constants;
+import org.namelessrom.updatecenter.items.UpdateInfo;
+
+public class DownloadService extends IntentService implements Constants {
+    private static final String TAG = DownloadService.class.getSimpleName();
+
+    private static final String EXTRA_UPDATE_INFO = "update_info";
+
+    public static void start(Context context, UpdateInfo ui) {
+        Intent intent = new Intent(context, DownloadService.class);
+        intent.putExtra(EXTRA_UPDATE_INFO, (Parcelable) ui);
+        context.startService(intent);
+    }
+
+    public DownloadService() {
+        super(TAG);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        final UpdateInfo ui = intent.getParcelableExtra(EXTRA_UPDATE_INFO);
+
+        if (ui != null) {
+            final long downloadId = enqueueDownload(ui.getUpdateUrl(), ui.getUpdateName());
+
+            PreferenceManager.getDefaultSharedPreferences(this)
+                    .edit().putLong(DOWNLOAD_ID, downloadId).commit();
+
+            final DownloadItem item = new DownloadItem(
+                    ui.getUpdateName() + ".zip", String.valueOf(downloadId),
+                    ui.getUpdateMd5(), "0");
+            DatabaseHandler.getInstance(this).insertOrUpdate(item, DatabaseHandler.TABLE_DOWNLOADS);
+            org.namelessrom.updatecenter.Application.mDownloadItems =
+                    DatabaseHandler.getInstance(this).getAllItems(DatabaseHandler.TABLE_DOWNLOADS);
+
+            Application.sHandler.post(new Runnable() {
+                @Override
+                public void run() {
+                    BusProvider.getBus().post(new RefreshEvent());
+                }
+            });
+        }
+    }
+
+    private long enqueueDownload(String downloadUrl, String updateName) {
+        final DownloadManager dm = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
+        DownloadManager.Request request = new DownloadManager.Request(Uri.parse(downloadUrl));
+
+        request.setTitle(getString(R.string.app_name));
+        request.setDescription(updateName);
+        request.setDestinationInExternalPublicDir(UPDATE_FOLDER, updateName + ".zip");
+        request.setAllowedOverRoaming(false);
+        request.setVisibleInDownloadsUi(false);
+
+        // TODO: this could/should be made configurable
+        request.setAllowedOverMetered(true);
+
+        return dm.enqueue(request);
+    }
+}
