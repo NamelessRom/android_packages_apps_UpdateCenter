07898ca192c37cf3230fd844b0f4953d5b951125
Evisceration <eviscerationls@gmail.com>
2014-05-12 14:38:45 +0200

    UC: drop custom ion copy
    Change-Id: Ia66f407364fd3ab0cc22e12d5b3fae304ecbadde

--- Android.mk
+++ Android.mk
@@ -1,3 +1,3 @@
 LOCAL_PATH := $(call my-dir)
 
-include $(LOCAL_PATH)/ion/Android.mk $(LOCAL_PATH)/ion/AndroidAsync/Android.mk $(LOCAL_PATH)/src/main/Android.mk
+include $(LOCAL_PATH)/src/main/Android.mk
