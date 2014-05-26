ada6cf1ab656298e5b70500751fce8c30b7268a3
Evisceration <eviscerationls@gmail.com>
2014-05-26 18:33:18 +0200

    UC: show croutons when on metered network or no network at all
    Change-Id: Iad1ecf3bca346817f4dc6b29607d014f66c386f9

--- src/main/java/org/namelessrom/updatecenter/fragments/updates/UpdateListFragment.java
+++ src/main/java/org/namelessrom/updatecenter/fragments/updates/UpdateListFragment.java
@@ -24,6 +24,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.text.format.Time;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -44,12 +45,16 @@ import org.namelessrom.updatecenter.items.UpdateInfo;
 import org.namelessrom.updatecenter.services.UpdateCheckService;
 import org.namelessrom.updatecenter.utils.BusProvider;
 import org.namelessrom.updatecenter.utils.Constants;
+import org.namelessrom.updatecenter.utils.Helper;
 import org.namelessrom.updatecenter.widgets.AttachListFragment;
 import org.namelessrom.updatecenter.widgets.adapters.UpdateListAdapter;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import de.keyboardsurfer.android.widget.crouton.Configuration;
+import de.keyboardsurfer.android.widget.crouton.Crouton;
+import de.keyboardsurfer.android.widget.crouton.Style;
 import uk.co.senab.actionbarpulltorefresh.library.ActionBarPullToRefresh;
 import uk.co.senab.actionbarpulltorefresh.library.PullToRefreshLayout;
 import uk.co.senab.actionbarpulltorefresh.library.listeners.OnRefreshListener;
@@ -82,6 +87,8 @@ public class UpdateListFragment extends AttachListFragment implements OnRefreshL
             }
         } catch (Exception ignored) { /* Not registered, nothing to do */ }
 
+        Crouton.cancelAllCroutons();
+
         BusProvider.getBus().unregister(this);
     }
 
@@ -112,6 +119,17 @@ public class UpdateListFragment extends AttachListFragment implements OnRefreshL
                 .theseChildrenArePullable(listView, listView.getEmptyView())
                 .listener(this)
                 .setup(mPullToRefreshLayout);
+
+        if (PreferenceManager
+                .getDefaultSharedPreferences(Application.sApplicationContext)
+                .getBoolean(Constants.PREF_UPDATE_METERED, true)
+                && Helper.isMetered(Application.sApplicationContext)) {
+            Crouton.makeText(getActivity(), R.string.metered_notice,
+                    new Style.Builder().setBackgroundColorValue(Style.holoBlueLight).build()
+            ).setConfiguration(
+                    new Configuration.Builder().setDuration(Configuration.DURATION_LONG).build()
+            ).show();
+        }
 
         checkForUpdates();
     }
@@ -159,7 +177,6 @@ public class UpdateListFragment extends AttachListFragment implements OnRefreshL
                 final Activity activity = getActivity();
                 if (activity != null) {
                     final UpdateListAdapter adapter = new UpdateListAdapter(activity, mTitles);
-
                     setListAdapter(adapter);
                     adapter.notifyDataSetChanged();
                 }
@@ -237,10 +254,16 @@ public class UpdateListFragment extends AttachListFragment implements OnRefreshL
                                         getString(R.string.data_connection_required))
                         );
                         getTitles();
+                        Crouton.makeText(getActivity(), R.string.data_connection_required,
+                                new Style.Builder().setBackgroundColorValue(Style.holoRedLight)
+                                        .build()
+                        ).setConfiguration(
+                                new Configuration.Builder().setDuration(Configuration.DURATION_LONG)
+                                        .build()
+                        ).show();
                     }
                 }
             });
         }
     }
-
 }
